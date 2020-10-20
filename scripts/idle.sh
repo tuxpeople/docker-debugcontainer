829bdf547e66ac747e0e4e33282ebf9fb8dce4a3
Thomas Deutsch <thomas@tuxpeople.org>
2020-10-20 11:50:21 +0200

    Update idle.sh

--- scripts/idle.sh
+++ scripts/idle.sh
@@ -1,5 +1,5 @@
 #!/bin/bash                                                                
-# https://gist.githubusercontent.com/ikbear/56b28f5ecaed76ebb0ca/raw/27561527483fb18399fb5d558dc55dd4c4839ed3/idle.sh
+# https://gist.github.com/ikbear/56b28f5ecaed76ebb0ca
 
 echo "This is a idle script (infinite loop) to keep container running."    
                                                                            
