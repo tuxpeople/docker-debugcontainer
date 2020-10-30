27fc84fac97df9a1c74df8968ac502ff91847173
Thomas Deutsch <thomas@tuxpeople.org>
2020-10-30 08:14:20 +0100

    Update docker-push-setup.sh

--- .travis/docker-push-setup.sh
+++ .travis/docker-push-setup.sh
@@ -6,9 +6,11 @@ export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRAN
 if [ "$BRANCH" == "master" ]; then
     echo "This commit has been merged to master so on success images will be pushed"
     export SHOULD_DOCKER_PUSH=true
+    export TAGS=latest
 else
     if [ "$BRANCH" == "devel" ]; then
         export SHOULD_DOCKER_PUSH=true
+        export TAGS=devel
     else
     echo "This is branch: $BRANCH so we won't be pushing"
     fi
@@ -17,3 +19,5 @@ fi
 if [ "$SHOULD_DOCKER_PUSH" = true ]; then
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 fi
+
+DOCKER_BASE=${DOCKER_USER}/$(echo ${TRAVIS_REPO_SLUG} | cut -d'/' -f2)
